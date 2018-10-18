d042d4415f569619534fa57b5d8c52f7548348ed
Felix Ho <felix.ho@nab.com.au>
2018-10-18 13:51:31 +1100

    Update Category with PUT

--- src/main/java/ho/felix/fhwebfluxrest/controllers/CategoryController.java
+++ src/main/java/ho/felix/fhwebfluxrest/controllers/CategoryController.java
@@ -35,4 +35,10 @@ public class CategoryController {
         return categoryRepository.saveAll(categoryStream).then();
     }
 
+    @PutMapping("/{id}")
+    Mono<Category> update(@PathVariable String id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
+    }
+
 }
